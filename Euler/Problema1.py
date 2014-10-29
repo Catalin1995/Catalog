490ec0c566742a974163414728b7877c8b7c8f72
Catalin1995 <muresan.ionut95@gmail.com>
2014-10-29 15:57:48 +0200

    Euler

--- Euler/Problema1.py
+++ Euler/Problema1.py
@@ -1,7 +1,8 @@
-#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-#Find the sum of all the multiples of 3 or 5 below 1000.
-
- def test_sumaMultipliNumar():
+'''
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+'''
+def test_sumaMultipliNumar():
     assert sumaMultipliNumar(3,10) == 18
     assert sumaMultipliNumar(5,10) == 5
     assert sumaMultipliNumar(10,0) == 0
@@ -10,7 +11,7 @@
 def sumaMultipliNumar(multiplu, numar):
     sumaMultipli = 0
     for i in range(multiplu, numar, multiplu):
-        sumaMultipli = sumaMultipli+ i
+        sumaMultipli += i
     return sumaMultipli
 
 def test_sumaMultipli():
@@ -22,9 +23,9 @@ def test_sumaMultipli():
     assert sumaMultipli(6) == 8
 
 def sumaMultipli(numar):
-    suma = 0
-    suma += sumaMultipliNumar(3,numar)
+    suma = sumaMultipliNumar(3,numar)
     suma += sumaMultipliNumar(5,numar)
+    suma -= sumaMultipliNumar(15,numar)
     return suma
 
 def main():
@@ -32,6 +33,6 @@ def main():
     numar = int(input())
     print("Suma multiplilor de 3 si de 5 pana la ", numar, " este:", sumaMultipli(numar))
 
-test_sumaMultipliNumar()
-test_sumaMultipli()
+#test_sumaMultipliNumar()
+#test_sumaMultipli()
 main()
