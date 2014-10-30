0679d664d6a3eb86fa25554a758e707b90c38e1c
Catalin1995 <muresan.ionut95@gmail.com>
2014-10-30 17:50:17 +0200

    Test Catalog

---
+++ testCatalog.py
@@ -0,0 +1,10 @@
+import catalog
+import os
+import unittest
+
+class catalogTestCase(unittest.TestCase):
+    def test_sum(self):
+        assert 1+1 == 2
+
+if (__name__ == '__main__'):
+    unittest.main()
