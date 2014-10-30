2abf65d2895fd268ab924ebdde95a0d05840e362
Catalin1995 <muresan.ionut95@gmail.com>
2014-10-30 15:41:09 +0200

    use heroku port environment variable

--- catalog.py
+++ catalog.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 DEBUG = True
@@ -11,4 +12,6 @@ def index(name=None):
     return render_template('index.html', name=name)
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 1337))
+    app.run(host='0.0.0.0', port=port)
     app.run()
