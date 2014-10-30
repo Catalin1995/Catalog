402bf99c4a7f31f2a176830b5049e0a0bc301529
Cristian Mircea Messel <cristian.messel@tecknoworks.com>
2014-10-30 11:46:54 +0200

    oh noes you found me

--- Catalog/catalog.py
+++ Catalog/catalog.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+@app.route('/send', methods=['POST'])
+def send():
+    return request.form['username']
 
 @app.route('/')
 @app.route('/<name>')
