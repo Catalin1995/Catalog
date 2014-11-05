1d1dcbef3db5413c3254ab1232834d699083b975
Cristian Mircea Messel <cristian.messel@tecknoworks.com>
2014-11-05 11:13:09 +0200

    add base test class

--- testCatalog.py
+++ testCatalog.py
@@ -2,6 +2,12 @@ import catalog
 import os
 import unittest
 
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        catalog.app.config['TESTING'] = True
+        self.app = catalog.app.test_client()
+
+
 class catalogTestCase(unittest.TestCase):
     def test_sum(self):
         assert 1+1 == 2
