798df94ba97f35a1e1769f8bcf1ede034fdcfdc1
Catalin1995 <muresan.ionut95@gmail.com>
2014-11-12 14:00:30 +0200

    Creare tabel cu elevi

--- Catalog/static/js/catalog.js
+++ Catalog/static/js/catalog.js
@@ -1,9 +1,10 @@
 var catalogApp = angular.module('catalogApp', []);
 
-catalogApp.controller('StudentListController', function ($scope) {
-  $scope.students = [
-    {'first_name': 'A', 'last_name': 'B'},
-    {'first_name': 'C', 'last_name': 'D'},
-    {'first_name': 'E', 'last_name': 'F'}
-  ];
+catalogApp.controller('StudentListController', function ($scope, $http) {
+  $http.get("/students.json").then(function (result) {
+  	console.log(result);
+  	$scope.students = result.data;
+  });
+
+  $scope.aux = 1;
 });
