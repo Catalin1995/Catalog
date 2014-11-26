d14e9cea9882aba198901798f18c7b1037f482dd
Catalin1995 <muresan.ionut95@gmail.com>
2014-11-26 15:52:26 +0200

    add absente and note

--- catalog/catalog.py
+++ catalog/catalog.py
@@ -74,13 +74,15 @@ def delete_students_id(id):
 
 class Student(Document):
 
-    first_name = StringField(max_length=50)
-    last_name = StringField(max_length=50)
-    clasa = StringField(max_length=50)
-    data_nasteri = StringField(max_length=50)
-    adresa = StringField(max_length=50)
-    alte_informati = StringField(max_length=50)
-
+    first_name = StringField(max_length=50)     #reprezinta numele elevului
+    last_name = StringField(max_length=50)      #reprezinta prenumele elevului
+    clasa = StringField(max_length=50)          #reprezinta clasa in care se afla elevul
+    data_nasteri = StringField(max_length=50)   #reprezinta data nasteri
+    adresa = StringField(max_length=50)         #reprezinta adresa elevului
+    alte_informati = StringField(max_length=50) #reprezinta alte informati despre elev
+    note = ListField()                          #reprezinta o lista cu stringuri de note
+    absente = ListField()                       #reprezinta o lista cu stringuri de absente
+    
     def update_student(self, new_stud):
 
         self.first_name = new_stud['first_name']
@@ -89,6 +91,8 @@ class Student(Document):
         self.data_nasteri = new_stud['data_nasteri']
         self.adresa = new_stud['adresa']
         self.alte_informati = new_stud['alte_informati']
+        self.note = new_stud['note']
+        self.absente = new_stud['absente']
 
     
 if __name__ == '__main__':
--- module_tests/testStudent.py
+++ module_tests/testStudent.py
@@ -23,6 +23,8 @@ class StudentTestCase(unittest.TestCase):
         student.data_nasteri = "27/10/1995"
         student.adresa = "Dorobantilor 90"
         student.alte_informati = "Telefon: 1"
+        student.note = ['2','3','4','5']
+        student.absente = ['10/10/2010', '20/20/2010', '30/30/2030']
         dimension = len(Student.objects)
         student.save()
         assert len(Student.objects) == dimension+1
@@ -35,6 +37,8 @@ class StudentTestCase(unittest.TestCase):
         student.data_nasteri = "11/04/2000"
         student.adresa = "Nasaud 19"
         student.alte_informati = "Telefon: 2"
+        student.note = ['2','3','10']
+        student.absente = ['10/20/3000', '20/30/1000']
         dimension = len(Student.objects)
         student.save()
         assert len(Student.objects) == dimension+1
@@ -43,8 +47,12 @@ class StudentTestCase(unittest.TestCase):
         nume = Student.objects(first_name="Muresan").count()
 
     def test_update_student(self):
+        note = ['2','3','4','5']
+        absente = ['10/10/2010', '20/20/2010', '30/30/2030']
         oldStud = Student('muresan', 'ionut', '12-A',
-                          '27/10/1995', 'Dorobantilor 90', 'Telefon: 1')
+                          '27/10/1995', 'Dorobantilor 90', 'Telefon: 1',
+                          note, 
+                          absente)
         newStud = {}
         newStud['first_name'] = 'dan'
         newStud['last_name'] = 'minteuan'
@@ -52,14 +60,19 @@ class StudentTestCase(unittest.TestCase):
         newStud['data_nasteri'] = '11/11/1111'
         newStud['adresa'] = 'Dorobantilor 1'
         newStud['alte_informati'] = 'Telefon: 2'
- 
+        newStud['note'] = ['2','3','4','5']
+        newStud['absente'] = ['10/10/2010', '20/20/2010', '30/30/2030']
+         
         oldStud.update_student(newStud)
+         
         assert oldStud.first_name == 'dan'
         assert oldStud.last_name == 'minteuan'
         assert oldStud.clasa == '11-A'
         assert oldStud.data_nasteri == '11/11/1111'
         assert oldStud.adresa == 'Dorobantilor 1'
         assert oldStud.alte_informati == 'Telefon: 2'
+        assert oldStud.note == ['2', '3', '4', '5']
+        assert oldStud.absente == ['10/10/2010', '20/20/2010', '30/30/2030']
 
 
 if (__name__ == '__main__'):
